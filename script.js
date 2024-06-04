03ad43d9ad42d3136ae50829b8839b24076c1690
Emiel Mac <112609864+EKlijnsma@users.noreply.github.com>
2024-06-04 15:32:22 +0200

    Create getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log(' hello world ')
+// create list of options
+const gameOptions = ['Rock', 'Paper', 'Scissors']
+
+function getComputerChoice() {
+    // get random number x between 0-2 (inclusive)
+    let x = Math.floor(Math.random() * 3);
+
+    // return index x of list y
+    return gameOptions[x];
+}
+
+console.log(getComputerChoice())
