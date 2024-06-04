f42c45cd1fb7a3b5a1cff65160c351be4475af17
Emiel Mac <112609864+EKlijnsma@users.noreply.github.com>
2024-06-04 16:08:07 +0200

    complete playRound function
    the playRound function plays 1 round with
    2 parameters as input and console logs
    an appropriate message to announce the result

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const gameOptions = ['rock', 'paper', 'scissors']
 let humanScore = 0;
 let computerScore = 0;
 
+// Function to get a random computer choice
 function getComputerChoice() {
     // get random number x between 0-2 (inclusive)
     let i = Math.floor(Math.random() * 3);
@@ -13,6 +14,32 @@ function getComputerChoice() {
     return gameOptions[i];
 }
 
+// Function to get the human players' choice
 function getHumanChoice() {
     return prompt("Type your choice: Rock, Paper or Scissors: ").toLowerCase()
 }
+
+// Function to play a round of RPS
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        // Identical choices, tie
+        console.log(`Its a tie! You both chose ${humanChoice}`)
+    } else if (
+        humanChoice === 'rock' && computerChoice === 'scissors' ||
+        humanChoice === 'paper' && computerChoice === 'rock' ||
+        humanChoice === 'scissors' && computerChoice === 'paper'
+    ) {
+        // Winning cases, human wins
+        humanScore++
+        console.log(`You win! ${humanChoice} beats ${computerChoice}!`)
+    } else {
+        // All other cases, human loses
+        computerScore++
+        console.log(`You lose! ${humanChoice} loses to ${computerChoice}!`)
+    }
+}
+
+let humanSelection = getHumanChoice();
+let computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection)
