0c8fa06f0fa0e8ddf45740746a306c8732061434
Emiel Mac <112609864+EKlijnsma@users.noreply.github.com>
2024-06-04 15:43:59 +0200

    Create getHumanChoice function

--- script.js
+++ script.js
@@ -3,10 +3,22 @@ const gameOptions = ['Rock', 'Paper', 'Scissors']
 
 function getComputerChoice() {
     // get random number x between 0-2 (inclusive)
-    let x = Math.floor(Math.random() * 3);
+    let i = Math.floor(Math.random() * 3);
 
     // return index x of list y
-    return gameOptions[x];
+    return gameOptions[i];
 }
 
+function getHumanChoice() {
+    // create input variable
+    // ask for input and store answer in variable
+    let i = parseInt(prompt("Type [0] for Rock, [1] for Paper or [2] for Scissors: "))
+
+    // verify that input is valid?
+    // if not valid, new iteration
+    // If valid, return value
+    return gameOptions[i]
+}
+
+console.log(getHumanChoice())
 console.log(getComputerChoice())
